d7dc949edd583e56671bae735d1f8730d3ca3281
Nixill <github@nixill.net>
2021-05-21 18:14:03 -0400

    Initial commit copied from ShadowKitchen
    Original: ShadowKitchen/f19c6b74c5f18a9ee3e7a6f4aea1064a96e1bea2

---
+++ src/main/java/net/nixill/fabric/shadowkitchen/ShadowKitchenItems.java
@@ -0,0 +1,33 @@
+package net.nixill.fabric.shadowkitchen;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ShadowKitchenItems {
+  private static final Map<Identifier, Item> ITEMS = new LinkedHashMap<>();
+
+  public static final Item FLOUR = add("flour", new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+  public static final Item DOUGH = add("dough", new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+  public static final Item BREAD_SLICE = add("bread_slice", new Item(
+      new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(1).snack().build())));
+  public static final Item YEAST = add("yeast", new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+  public static final Item CRUSH_STONE = add("crush_stone", new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+  private static <I extends Item> I add(String name, I item) {
+    ITEMS.put(new Identifier("shadow_refinery", name), item);
+    return item;
+  }
+
+  public static void registerItems() {
+    for (Identifier id : ITEMS.keySet()) {
+      Registry.register(Registry.ITEM, id, ITEMS.get(id));
+    }
+  }
+}
---
+++ src/main/java/net/nixill/fabric/shadowkitchen/ShadowKitchenMod.java
@@ -0,0 +1,15 @@
+package net.nixill.fabric.shadowkitchen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ShadowKitchenMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world!");
+		ShadowKitchenItems.registerItems();
+	}
+}
